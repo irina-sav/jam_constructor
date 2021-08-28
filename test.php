856aec41dee04ee0889bdacd842b4527b152b80b
Irina_Dunkerk <savelyeva.irin@yandex.ru>
2021-08-28 10:35:00 +0300

    continue php massives, funtions

--- test.php
+++ test.php
@@ -1,15 +1,60 @@
 <?php
-require 'test_controller.php';
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-   <h1> <?= $name?> </h1> 
-</body>
-</html>
+// $items = array("яблоко", "груша", "вишня");
+// $items = ["яблоко", 
+// "груша",
+//  "вишня" =>
+//  ["сладкая вишня", "кислая вишня"],
+// ];
+$items = [
+    "apple" => "яблоко",
+    "pear" => "груша",
+    "cherry" => "вишня",
+];
+// echo $items["apple"];
+// $counter = 0;
+// $number = 1;
+// while ($counter < count($items)) {
+//     echo  "$number) {$items[$counter]} <br>";
+//     $counter ++;
+//     $number ++;
+// }
+// for ($counter = 0, $number = 1; $counter < count($items); $counter ++, $number = $counter + 1) {
+//     echo "{$number}) {$items[$counter]}<br/>";
+// }
+
+$fruits = [
+    "apple" => [
+        "name" => "яблоко", 
+        "price" => 100, 
+        "color" => "red", 
+        "quantity" => 8,
+    ],
+    "pear" => [
+        "name" => "груша", 
+        "price" => 50, 
+        "color" => "yellow", 
+        "quantity" => 56,
+    ],
+    "cherry" => [
+        "name" => "вишня", 
+        "price" => 39, 
+        "color" => "brown", 
+        "quantity" => 96,
+    ],
+];
+
+
+function printCode($code) {
+    echo "<pre>";
+    print_r($code);
+    echo "</pre>";
+};
+// printCode($fruits);
+
+foreach($fruits as $keyFruit => $oneFruit) {
+    foreach($oneFruit as $keyFruitParam => $oneFruitParam)
+    printCode($oneFruitParam);
+}
+
+
+
