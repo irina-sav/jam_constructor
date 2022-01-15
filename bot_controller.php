f9bbb899fee863e5b50c605d70eca6f37592b874
Irina_Dunkerk <savelyeva.irin@yandex.ru>
2022-01-15 12:39:30 +0300

    telegram bot dev

--- bot_controller.php
+++ bot_controller.php
@@ -1,8 +1,19 @@
 <?php
+$input = file_get_contents('php://input');
 require_once 'config.php';
 require_once 'bot_model.php';
+// setWebHook();
+
+$inputArray = json_decode($input, true);
+addlog($inputArray);
+if ($chatId = @$inputArray['message']['chat']['id']) {
+    requestToTelegramAPI('sendMessage', [
+        'chat_id' => $chatId,
+        'text' => 'Вы написали' . ' ' . @$inputArray['message']['text'],
+    ]);
+}
 // $botMethod = 'getUpdates';
 $messageMethod = 'sendMessage';
 $sms = 'Hello, Irina';
 
-print_r(requestToTelegramAPI($messageMethod, ['text' => $sms]));
+// print_r(requestToTelegramAPI($messageMethod, ['text' => $sms]));
--- bot_model.php
+++ bot_model.php
@@ -1,11 +1,36 @@
 <?php
 function requestToTelegramAPI(string $botMethod, array $options = []): string
 {
-    $options['chat_id'] = ADMIN_CHAT_ID;
+    $options['chat_id'] = @$options['chat_id'] ?? ADMIN_CHAT_ID;
     $apiUrl =
         'https://api.telegram.org/bot' .
         BOT_TOKKEN .
         "/{$botMethod}?" .
         http_build_query($options);
     return file_get_contents($apiUrl);
+}
+
+function setWebHook(
+    bool $webHookSet = true,
+    string $path = 'bot_controller.php'
+) {
+    // if ($webHookSet) {
+    //     $url = "{$_SERVER['HTTP_X_FORWARDED_PROTO']}://{$_SERVER['HTTP_HOST']}/{$path}";
+    // } else {
+    //     $url = '';
+    // }
+
+    $url = $webHookSet
+        ? "{$_SERVER['HTTP_X_FORWARDED_PROTO']}://{$_SERVER['HTTP_HOST']}/{$path}"
+        : '';
+
+    $requestAPI = requestToTelegramAPI('setWebhook', ['url' => $url]);
+    exit($requestAPI);
+}
+
+function addLog($info)
+{
+    $date = date('d.m.Y H:i:s');
+    $oneLog = $date . ' ==> ' . print_r($info, true) . "\n";
+    file_put_contents('logs/log.txt', $oneLog, FILE_APPEND);
 }
