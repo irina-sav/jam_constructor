4cca682484e1e36f20386ad07621a72bcbb819d9
Irina_Dunkerk <savelyeva.irin@yandex.ru>
2022-03-06 09:42:21 +0300

    cleaning branch master

--- test_site/loger.php
+++
@@ -1,7 +0,0 @@
-<?php
-function addLog($info) {
-    $date = date("d.m.Y H:i:s");
-    $oneLog = $date . " ==> " . print_r($info, true) . "\n";
-    file_put_contents("logs/log.txt", $oneLog, FILE_APPEND);
-   
-}
